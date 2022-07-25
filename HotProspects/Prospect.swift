022b61576bdeb4618f35adefdb06a26a07c8cffd
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-25 10:25:26 +0200

    Changed the saving method from UserDefaults to Documents

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -18,15 +18,15 @@ class Prospect: Identifiable, Codable {
     @Published private(set) var people: [Prospect]
     
     let saveKey = "SavedData"
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedData")
     
     init() {
-        if let data = UserDefaults.standard.data(forKey: saveKey) {
-            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
-                people = decoded
-                return
-            }
+        do {
+            let data = try Data(contentsOf: savePath)
+            people = try JSONDecoder().decode([Prospect].self, from: data)
+        } catch {
+            people = []
         }
-        people = []
     }
     
     func toggle(_ prospect: Prospect) {
@@ -36,8 +36,11 @@ class Prospect: Identifiable, Codable {
     }
     
     private func save() {
-        if let encoded = try? JSONEncoder().encode(people) {
-            UserDefaults.standard.set(encoded, forKey: saveKey)
+        do {
+            let data = try JSONEncoder().encode(people)
+            try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+        } catch {
+            print("Saving data has failed.")
         }
     }
     
